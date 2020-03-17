b6af90715dfff40aeb384b27837eed2b40917eeb
Yehor <y.khohlov@pnn.in.ua>
2020-03-17 12:44:10 +0200

    Initial commit

---
+++ src/main/kotlin/com/parsing/marketobserverserver/MarketObserverServerApplication.kt
@@ -0,0 +1,11 @@
+package com.parsing.marketobserverserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MarketObserverServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<MarketObserverServerApplication>(*args)
+}
---
+++ src/test/kotlin/com/parsing/marketobserverserver/MarketObserverServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.parsing.marketobserverserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MarketObserverServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
